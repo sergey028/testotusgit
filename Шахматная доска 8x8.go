8b9cfc1cf447ae4d1499a58436472fd7f9824f31
Sergey Shurdakov <48182143+sergey028@users.noreply.github.com>
2025-07-13 06:49:54 +0300

    Update Шахматная доска 8x8.go
    Added fmt.Scan

--- Шахматная доска 8x8.go
+++ Шахматная доска 8x8.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	size := 8
+	var size int
 
+	fmt.Print("Введите размер шахматной доски: ")
+	_, err := fmt.Scan(&size)
+
+	if err != nil {
+		fmt.Println("Ошибка", err)
+		return
+	}
+	//fmt.Println(sizeOfDesk)
 	for i := 0; i < size; i += 1 {
+
 		for j := 0; j < size; j += 1 {
 
 			if (i+j)%2 == 0 {
@@ -15,6 +24,7 @@ func main() {
 			}
 		}
 		fmt.Println()
+
 	}
 
 }
